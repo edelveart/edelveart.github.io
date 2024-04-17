0947666c9846c1e026fa4c9ab5000f3db23f0d4c
Edgar Delgado Vega <edelve91@gmail.com>
2024-04-17 14:07:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website
+
+Based on  <a href="https://astrofy-template.netlify.app/" target="_blank" class="font-bold">Astrofy Template ⚡️</a>
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'EdelveArt | Music, Code and Math';
+export const SITE_DESCRIPTION = 'Personal website of Edgar Armando Delgado Vega, musician and artist with a love for science and technology. I have 15 years of experience as a musical performer and 10 years of experience in music teaching, at all educational levels ( primary, secondary, higher and postgraduate). I have taught both in person and virtually. In recent years I have taught basic music programming courses.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
