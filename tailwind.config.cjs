ec53b48a452fe485cf2463903210a536882d8517
Edgar Delgado Vega <edelve91@gmail.com>
2024-05-06 19:33:28 -0500

    feat: add new Icon and fixes typos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
 
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
-	daisyui: {
-		themes: ['synthwave'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ['coffee'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+  }
 
 }
 
