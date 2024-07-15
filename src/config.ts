75db46fbc5c18886106f18e37cd8239fd3391b33
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-15 02:55:32 -0500

    feat: update bio

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Edgar Delgado Vega | Music, Code and Math"
+export const SITE_TITLE = "Edgar Delgado Vega | Code, Math and Music"
 export const SITE_DESCRIPTION =
   "Personal website of Edgar Armando Delgado Vega, musician and artist with a love for science and technology. I have 15 years of experience as a musical performer and 10 years of experience in music teaching, at all educational levels ( primary, secondary, higher and postgraduate). I have taught both in person and virtually. In recent years I have taught basic music programming courses."
 export const GENERATE_SLUG_FROM_TITLE = true
