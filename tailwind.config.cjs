5ecf5a7dc52bf7c32a8821bde5b79221049acd6b
Edgar Delgado Vega <edelve91@gmail.com>
2024-08-16 04:43:18 -0500

    feat: update Bio and change DaisyUI Theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
 
+import defaultTheme from "tailwindcss/defaultTheme"
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
     extend: {
       fontFamily: {
-        sans: ['Iosevka', ...defaultTheme.fontFamily.mono],
+        sans: ["Iosevka", ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ['sunset'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["forest"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-  }
-
+  },
 }
 
 // [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]
