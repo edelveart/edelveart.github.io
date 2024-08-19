248b4f70e37ce1333a4d5f2f5de79495b84f4035
Edgar Delgado Vega <edelve91@gmail.com>
2024-08-19 18:47:22 -0500

    Revert "feat: add toggle theme in header"
    This reverts commit 2f0e5b8ba3c7f6195a1b088526a88550873f686d.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,8 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["autumn", "sunset"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    // darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: ["autumn"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
     logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
   },
 }
