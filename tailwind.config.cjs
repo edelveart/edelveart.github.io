4fa97a8a6c0682f8ff12f36bc8e42604a8a3ba7d
Edgar Delgado Vega <edelve91@gmail.com>
2024-10-29 19:47:01 -0500

    fix: rewrite bio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,8 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["dark"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "coffee", // name of one of the included themes for dark mode
+    themes: ["dim"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "sunset", // name of one of the included themes for dark mode
     logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
   },
 };
