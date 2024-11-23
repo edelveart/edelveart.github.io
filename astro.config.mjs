848c942e07ff14e1556141a7dd4b8290987648ec
Edgar Delgado Vega <edelve91@gmail.com>
2024-11-23 00:07:50 -0500

    feat: change BaseLayout lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edelveart.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-  },
+  // i18n: {
+  //   defaultLocale: "es",
+  //   locales: ["es", "en"],
+  // },
   plugins: [
     sitemap({
       // Routes of SiteMap excluded
-      exclude: ["/services/", "/store/", "/blog/"],
+      exclude: ["/services/", "/store/"],
     }),
   ],
-})
+});
