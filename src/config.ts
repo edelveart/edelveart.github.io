616934d5b5fd41a0a3ff953706470efafbf4620c
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-12 20:29:42 -0500

    chore(config.ts): update SITE_DESCRIPTION

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = "Edgar Delgado Vega";
 export const SITE_DESCRIPTION =
-  "TypeScript developer with knowledge of discrete algorithms and design patterns. In 2023, I launched ts-tonnetz, a package for algorithmic composition now used by Topos. Passionate about mathematics, I authored a Scopus-indexed article and developed the libraries fignumbers and figurate_numbers, the latter having over 2500 downloads. In addition to teaching at the postgraduate level, I collaborate on international open-source software projects.";
+  "Software developer passionate about problem-solving through mathematical abstractions, discrete algorithms, and design patterns. I launched ts-tonnetz, an advanced package for algorithmic composition and live coding, currently integrated into Topos. My figurate number libraries have surpassed 10,000 downloads. Previously, I published a Scopus-indexed paper on Mathematical Music Theory. In addition to teaching at the postgraduate level, I collaborate on international open-source projects.";
 
 export const SITE_URL = "https://edelveart.github.io/";
 export const GENERATE_SLUG_FROM_TITLE = true;
