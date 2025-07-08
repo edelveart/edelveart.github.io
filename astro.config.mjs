512efbcbad8727dfead0c0e0bb544a0a9a0d2b8d
Edgar Delgado Vega <edelve91@gmail.com>
2025-07-07 20:57:18 -0500

    fix(astro-config): solve problem with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,3 @@
-// import { defineConfig, passthroughImageService } from "astro/config";
-// import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
-// import tailwind from "@astrojs/tailwind";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   site: "https://edelveart.github.io",
-//   integrations: [
-//     mdx(),
-//     sitemap({
-//       filter: (page) =>
-//         page !== "https://edelveart.github.io/blog/" &&
-//         page !== "https://edelveart.github.io/store/" &&
-//         page !== "https://edelveart.github.io/store/item1/" &&
-//         page !== "https://edelveart.github.io/store/item2/" &&
-//         page !== "https://edelveart.github.io/store/item3/" &&
-//         page !== "https://edelveart.github.io/blog/tag/" &&
-//         page !== "https://edelveart.github.io/blog/funciones-currificadas-en-typescript/" &&
-//         page !== "https://edelveart.github.io/blog/tag/typescript/" &&
-//         page !== "https://edelveart.github.io/services/",
-//     }),
-//     tailwind(),
-//   ],
-//   image: {
-//     service: passthroughImageService(),
-//   },
-// });
-
 import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
