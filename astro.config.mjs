104547eb67cc51792a0f98de0a6dd6b2ed5f1607
Edgar Delgado Vega <edelve91@gmail.com>
2025-07-07 20:55:06 -0500

    fix(astro-config): solve problem with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,32 @@
+// import { defineConfig, passthroughImageService } from "astro/config";
+// import mdx from "@astrojs/mdx";
+// import sitemap from "@astrojs/sitemap";
+// import tailwind from "@astrojs/tailwind";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: "https://edelveart.github.io",
+//   integrations: [
+//     mdx(),
+//     sitemap({
+//       filter: (page) =>
+//         page !== "https://edelveart.github.io/blog/" &&
+//         page !== "https://edelveart.github.io/store/" &&
+//         page !== "https://edelveart.github.io/store/item1/" &&
+//         page !== "https://edelveart.github.io/store/item2/" &&
+//         page !== "https://edelveart.github.io/store/item3/" &&
+//         page !== "https://edelveart.github.io/blog/tag/" &&
+//         page !== "https://edelveart.github.io/blog/funciones-currificadas-en-typescript/" &&
+//         page !== "https://edelveart.github.io/blog/tag/typescript/" &&
+//         page !== "https://edelveart.github.io/services/",
+//     }),
+//     tailwind(),
+//   ],
+//   image: {
+//     service: passthroughImageService(),
+//   },
+// });
+
 import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -9,14 +38,13 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap({
-      filter: (page) =>
-        page !== "https://edelveart.github.io/blog/" &&
-        page !== "https://edelveart.github.io/store/" &&
-        page !== "https://edelveart.github.io/store/item1/" &&
-        page !== "https://edelveart.github.io/store/item2/" &&
-        page !== "https://edelveart.github.io/store/item3/" &&
-        page !== "https://edelveart.github.io/blog/tag/" &&
-        page !== "https://edelveart.github.io/services/",
+      filter: (page) => {
+        return (
+          !page.startsWith("https://edelveart.github.io/blog/") &&
+          !page.startsWith("https://edelveart.github.io/store/") &&
+          !page.startsWith("https://edelveart.github.io/services/")
+        );
+      },
     }),
     tailwind(),
   ],
