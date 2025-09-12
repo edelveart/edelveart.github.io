46858e56e42cbe9a57d686e3003333d79b23539a
Edgar Delgado Vega <edelve91@gmail.com>
2025-09-12 00:53:59 -0500

    style(website): change base font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,9 +9,9 @@ module.exports = {
       fontFamily: {
         // sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
         // sans: ["IBM Plex Sans Variable", ...defaultTheme.fontFamily.sans],
-        sans: ["Rubik Variable", ...defaultTheme.fontFamily.sans],
+        // sans: ["Rubik Variable", ...defaultTheme.fontFamily.sans],
         // sans: ["Manrope Variable", ...defaultTheme.fontFamily.sans],
-        // sans: ["Sora Variable", ...defaultTheme.fontFamily.sans],
+        sans: ["Sora Variable", ...defaultTheme.fontFamily.sans],
         // sans: ["Space Grotesk Variable", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono Variable", ...defaultTheme.fontFamily.mono],
         // sans: ["Ubuntu Sans Variable", ...defaultTheme.fontFamily.sans],
