02d64919953336fa8bf24bedfc536a88d13003c2
Edgar Delgado Vega <edelve91@gmail.com>
2025-09-18 15:28:28 -0500

    chore(robots): enable /blog

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Edgar Delgado Vega";
+export const SITE_TITLE = "Edgar Delgado Vega | Math and Creative Systems Developer";
 export const SITE_DESCRIPTION =
   "I design creative systems that integrate mathematical structures with computational tools for live coders, musicians, and curious minds. I currently develop modular forms, inspired by SageMath and LMFDB, and ts-tonnetz, two packages for real-time algorithmic composition. Moreover, my libraries for generating multidimensional figurate numbers have surpassed 17,000 downloads. I have published an article indexed in Scopus on Mathematical Music Theory, teach at the postgraduate level, and contribute to international open-source projects.";
 
