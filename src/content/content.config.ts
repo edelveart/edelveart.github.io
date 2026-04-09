6c2c70d04c9ae9acfd8d5fa5fffde1f92e362d1e
Edgar Delgado Vega <edelve91@gmail.com>
2026-04-08 23:35:14 -0500

    feat!: upgrade to Astro 6, set deploy to node v24 (blog config reformulated)2

--- src/content/content.config.ts
+++ src/content/content.config.ts
@@ -2,24 +2,6 @@ import { defineCollection } from "astro:content";
 import { glob, file } from "astro/loaders";
 import { z } from "astro/zod";
 
-// const blog = defineCollection({
-//   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     pubDate: z.coerce.date(),
-//     updatedDate: z.string().optional(),
-//     heroImage: z.string().optional(),
-//     badge: z.string().optional(),
-//     tags: z
-//       .array(z.string())
-//       .refine((items) => new Set(items).size === items.length, {
-//         message: "tags must be unique",
-//       })
-//       .optional(),
-//   }),
-// });
-
 const blogSchema = z.object({
   title: z.string(),
   description: z.string(),
@@ -35,26 +17,26 @@ const blogSchema = z.object({
     .optional(),
 });
 
-// const storeSchema = z.object({
-//   title: z.string(),
-//   description: z.string(),
-//   custom_link_label: z.string(),
-//   custom_link: z.string().optional(),
-//   updatedDate: z.coerce.date(),
-//   pricing: z.string().optional(),
-//   oldPricing: z.string().optional(),
-//   badge: z.string().optional(),
-//   checkoutUrl: z.string().optional(),
-//   heroImage: z.string().optional(),
-// });
+const storeSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  custom_link_label: z.string(),
+  custom_link: z.string().optional(),
+  updatedDate: z.coerce.date(),
+  pricing: z.string().optional(),
+  oldPricing: z.string().optional(),
+  badge: z.string().optional(),
+  checkoutUrl: z.string().optional(),
+  heroImage: z.string().optional(),
+});
 
 export type BlogSchema = z.infer<typeof blogSchema>;
-// export type StoreSchema = z.infer<typeof storeSchema>;
+export type StoreSchema = z.infer<typeof storeSchema>;
 
 const blogCollection = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   schema: blogSchema,
 });
-// const storeCollection = defineCollection({ schema: storeSchema });
+const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = { blogCollection };
