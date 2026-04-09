88bb42664ac5c0ea76a79a66d7a07f7c7a57636b
Edgar Delgado Vega <edelve91@gmail.com>
2026-04-09 00:04:26 -0500

    feat!: upgrade to Astro 6, set deploy to node v24 (blog config reformulated)-5

--- src/lib/createSlug.ts
+++ src/lib/createSlug.ts
@@ -1,19 +1,34 @@
-// Adapted from https://equk.co.uk/2023/02/02/generating-slug-from-title-in-astro/
+// // Adapted from https://equk.co.uk/2023/02/02/generating-slug-from-title-in-astro/
 
-import { GENERATE_SLUG_FROM_TITLE } from '../config'
+// import { GENERATE_SLUG_FROM_TITLE } from '../config'
 
-export default function (title: string, staticSlug: string) {
-  return (
-    !GENERATE_SLUG_FROM_TITLE ? staticSlug : title
-      // remove leading & trailing whitespace
-      .trim()
-      // output lowercase
-      .toLowerCase()
-      // replace spaces
-      .replace(/\s+/g, '-')
-      // remove special characters
-      .replace(/[^\w-]/g, '')
-      // remove leading & trailing separtors
-      .replace(/^-+|-+$/g, '')
-  )
+// export default function (title: string, staticSlug: string) {
+//   return (
+//     !GENERATE_SLUG_FROM_TITLE ? staticSlug : title
+//       // remove leading & trailing whitespace
+//       .trim()
+//       // output lowercase
+//       .toLowerCase()
+//       // replace spaces
+//       .replace(/\s+/g, '-')
+//       // remove special characters
+//       .replace(/[^\w-]/g, '')
+//       // remove leading & trailing separtors
+//       .replace(/^-+|-+$/g, '')
+//   )
+// }
+
+import { GENERATE_SLUG_FROM_TITLE } from "../config";
+
+export default function createSlug(title?: string, staticSlug?: string) {
+  if (!GENERATE_SLUG_FROM_TITLE) return staticSlug ?? "untitled";
+
+  if (!title) return staticSlug ?? "untitled";
+
+  return title
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]/g, "")
+    .replace(/^-+|-+$/g, "");
 }
