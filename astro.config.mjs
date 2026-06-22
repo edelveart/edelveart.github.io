788b4b941483448f48aad2c6230b430bf884a5ad
Edgar Delgado Vega <edelve91@gmail.com>
2026-06-22 16:13:21 -0500

    style(global): set theme to catppuccin mocha

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig({
       rehypePlugins: [rehypeKatex], // KaTeX
     }),
     shikiConfig: {
-      theme: "horizon",
+      theme: "catppuccin-mocha",
       wrap: false, //code wrap
     },
   },
