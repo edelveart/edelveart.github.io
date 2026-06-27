f7fa82e86c341e58a9c0665116920da38742d368
Edgar Delgado Vega <edelve91@gmail.com>
2026-06-27 15:19:15 -0500

    perf(header): remove blur effect to improve speed render2

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-  "Edgar Delgado Vega | Creative Math Systems Developer";
+export const SITE_TITLE = "Edgar Delgado Vega | Creative Math Systems Developer";
 export const SITE_DESCRIPTION =
   " I develop creative systems that merge mathematics, computation, and music. My work seeks correspondences through structural analogies. My current projects include modular forms (SageMath/LMFDB-inspired) and ts-tonnetz, both for real-time algorithmic composition, as well as multidimensional figurate number generators with over 23,000 downloads. I have published an article indexed in Scopus on Mathematical Music Theory, contribute to international open-source projects, and teach postgraduate courses. ";
 
 export const SITE_URL = "https://edelveart.github.io/";
 export const GENERATE_SLUG_FROM_TITLE = true;
-export const TRANSITION_API = true;
+export const TRANSITION_API = false;
 
 export const SOCIAL_MEDIA = {
   linkedin: "https://www.linkedin.com/in/edgararmandodelgadovega/?locale=en_US",
