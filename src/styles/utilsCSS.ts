ecb37b0c14491a88ffafdd4037837a454751a6c9
Edgar Delgado Vega <edelve91@gmail.com>
2026-07-02 19:49:44 -0500

    feat(svg-logo): add figuratenum-icon2

--- src/styles/utilsCSS.ts
+++ src/styles/utilsCSS.ts
@@ -3,13 +3,13 @@ export const NAV_LINKS_PAGES = {
     "group flex w-full min-h-24 items-center gap-3 rounded-md px-3 py-2 transition-colors duration-150 no-underline",
   prevLeft: "justify-start",
   nextRight: "justify-end",
-  prevNext: "text-sm uppercase traking-wide  text-base-content/60",
+  prevNext: "text-sm uppercase traking-wide text-base-content/60 font-normal",
   pageTitle:
     "text-base line-clamp-2 transition-colors duration-150 group-hover:text-primary group-hover:underline underline-offset-2",
   arrowSpan: "transition-colors duration-150 group-hover:text-primary",
 };
 
 export const META_BADGE_TAGS = {
-  badge: "text-sm text-info/70 tracking-wider uppercase font-medium",
-  tags: "text-sm text-info/70 hover:text-info/100 transition-colors underline-offset-2",
+  badge: "text-sm text-info/70 tracking-wider uppercase",
+  tags: "text-sm text-info/70 hover:text-info/100 transition-colors underline-offset-2 font-normal traking-wide",
 };
