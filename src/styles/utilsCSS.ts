9e77fbf811d6d9303404cea69e1fe81d9d73eede
Edgar Delgado Vega <edelve91@gmail.com>
2026-07-11 02:24:27 -0500

    style: set source-code-pro

--- src/styles/utilsCSS.ts
+++ src/styles/utilsCSS.ts
@@ -3,15 +3,16 @@ export const NAV_LINKS_PAGES = {
     "group flex w-full min-h-24 items-center gap-3 rounded-md px-3 py-2 transition-colors duration-150 no-underline",
   prevLeft: "justify-start",
   nextRight: "justify-end",
-  prevNext: "text-sm uppercase traking-wide text-base-content/60 font-normal",
+  prevNext: "text-sm uppercase traking-wide text-base-content/65 font-normal",
   pageTitle:
     "text-base line-clamp-2 transition-colors duration-150 group-hover:text-primary group-hover:underline underline-offset-2 font-normal",
   arrowSpan: "transition-colors duration-150 group-hover:text-primary",
 };
 
 export const META_BADGE_TAGS = {
-  badge: "text-sm text-info/70 tracking-wider uppercase font-[425]",
-  tags: "text-sm text-info/70 hover:text-info/100 transition-colors underline-offset-2 traking-wide font-[425]",
+  badge: "text-sm text-info/75 tracking-wider uppercase font-normal",
+  tags: "text-sm text-info/75 hover:text-info/100 transition-colors underline-offset-2 traking-wide font-normal hover:decoration-2",
 };
 
-export const LINKS_TO = "text-primary underline  underline-offset-2 hover:decoration-2";
+export const LINKS_TO =
+  "text-primary underline transition-colors underline-offset-2 hover:decoration-2";
