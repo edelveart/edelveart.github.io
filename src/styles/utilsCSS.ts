44d19d88051acb70f7ed647eca151b0726c966d7
Edgar Delgado Vega <edelve91@gmail.com>
2026-07-13 15:32:05 -0500

    feat: prev-2

--- src/styles/utilsCSS.ts
+++ src/styles/utilsCSS.ts
@@ -3,18 +3,18 @@ export const NAV_LINKS_PAGES = {
     "group flex w-full min-h-24 items-center gap-3 rounded-md px-3 py-2 transition-colors duration-150 no-underline",
   prevLeft: "justify-start",
   nextRight: "justify-end",
-  prevNext: "text-sm uppercase traking-wide text-base-content/65 font-normal",
+  prevNext: "text-sm uppercase traking-wide text-base-content/60 font-normal",
   pageTitle:
     "text-base line-clamp-2 transition-colors duration-150 group-hover:text-primary group-hover:underline underline-offset-2 font-normal",
   arrowSpan: "transition-colors duration-150 group-hover:text-primary",
 };
 
 export const META_BADGE_TAGS = {
-  badge: "text-sm text-info/75 tracking-wide uppercase font-medium",
-  tags: "text-sm text-info/75 hover:text-info/100 transition-colors underline-offset-2 traking-wide font-normal hover:decoration-2",
+  badge: "text-sm text-info/60 tracking-wide uppercase font",
+  tags: "text-sm text-info/60 hover:text-info/100 transition-colors underline-offset-2 traking-wide font-normal hover:decoration-2",
 };
 
 export const LINKS_TO =
   "text-primary underline transition-colors underline-offset-2 hover:decoration-2";
 
-export const BORDER_INFO = "border-l-4 pl-4 border-info/40";
+export const BORDER_INFO = "border-l-[2.5px] pl-3 border-info/40";
