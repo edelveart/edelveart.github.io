71dc1c0660e8385158c49c36b96e22ea60ee0dc5
Edgar Delgado Vega <edelve91@gmail.com>
2026-07-13 23:17:03 -0500

    feat: prev-7

--- src/styles/catppuccinTheme.mocha.ts
+++ src/styles/catppuccinTheme.mocha.ts
@@ -2,7 +2,10 @@ import { createCatppuccinPlugin } from "@catppuccin/daisyui";
 
 export default createCatppuccinPlugin(
   "mocha",
-  {},
+  {
+    primary: "lavender",
+    info: "flamingo",
+  },
   {
     default: true,
   },
