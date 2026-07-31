5b314b5bda1d7b7425c0de508d2e5e048266d347
Edgar Delgado Vega <edelve91@gmail.com>
2026-07-30 19:45:37 -0500

    feat(blog): update sonic-pi-v5-release misc-note + table of contents

--- src/styles/utilsCSS.ts
+++ src/styles/utilsCSS.ts
@@ -1,9 +1,9 @@
 export const NAV_LINKS_PAGES = {
   aLink:
-    "group flex w-full min-h-24 items-center gap-3 rounded-md px-3 py-2 transition-colors duration-150 no-underline",
+    "group flex w-full min-h-20 items-center gap-3 rounded-md px-3 py-2 transition-colors duration-150 no-underline",
   prevLeft: "justify-start",
   nextRight: "justify-end",
-  prevNext: "text-sm uppercase traking-wide text-base-content/65 font-light",
+  prevNext: "text-sm uppercase tracking-wide text-base-content/65 font-light",
   pageTitle:
     "text-base line-clamp-2 transition-colors duration-150 group-hover:text-primary group-hover:underline underline-offset-2 group-hover:decoration-2 font-normal",
 
