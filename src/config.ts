54dfe7221cd9e1067427dcca6b6e4edb9710866e
Edgar Delgado Vega <edelve91@gmail.com>
2026-08-05 13:07:22 -0500

    feat: CHANGE-NAVBAR-5

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Edgar Delgado Vega | Creative Math Systems Developer";
+export const SITE_TITLE =
+  "Edgar Delgado Vega | Creative Math Systems Developer";
 export const SITE_DESCRIPTION =
   "I develop creative systems that merge mathematics, computation, and music through structural analogies. All for real-time algorithmic composition, my current projects include modular forms (SageMath/LMFDB-inspired) and multidimensional figurate number libraries with over 26,000 downloads, as well as ts-tonnetz. I have published a Scopus-indexed article in Mathematical Music Theory, serve as a peer reviewer for The European Physical Journal Special Topics, contribute to international open-source projects, and teach postgraduate courses.";
 
 export const SITE_URL = "https://edelveart.github.io/";
 export const GENERATE_SLUG_FROM_TITLE = true;
-export const TRANSITION_API = true;
+export const TRANSITION_API = false;
 
 export const SOCIAL_MEDIA = {
   linkedin: "https://www.linkedin.com/in/edgararmandodelgadovega/?locale=en_US",
